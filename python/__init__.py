c370e41ebf6d95b81b5dd1bca16cb7e9a17c7b02
jf712@Reiankyo <jzzzf712@gmail.com>
2017-11-09 00:16:02 +0900

    Hello World_of_Tanks

---
+++ python/__init__.py
@@ -0,0 +1,16 @@
+XFW_MOD_INFO = {
+    # mandatory
+    'VERSION':       '0.9.20.1.2',
+    'URL':           'https://twitter.com/jf_nights',
+    'UPDATE_URL':    'https://twitter.com/jf_nights',
+    'GAME_VERSIONS': ['0.9.20.1.2'],
+}
+
+import random
+import BigWorld
+import GUI
+
+hello = GUI.Text("Hello World_of_Tanks!!!!!!!")
+hello.font = "WARGothic"
+GUI.addRoot(hello)
+
